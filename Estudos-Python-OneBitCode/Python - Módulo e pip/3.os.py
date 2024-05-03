e8f64942359b5b7ca2afa91e66efdf271b404ddd
Jhonatan Fagundes <149586964+Jhonatanfagundess@users.noreply.github.com>
2024-05-02 21:55:35 -0300

    estudos

---
+++ Estudos-Python-OneBitCode/Python - Módulo e pip/3.os.py
@@ -0,0 +1,22 @@
+import os
+
+# 1 - consultar funcionalidade
+#help('os')
+
+# 2 - retornar a pasta atual
+print(os.getcwd())
+
+# 3 - listar arquivos e pasta
+print(os.listdir())
+
+# 4 - apresentar a versão OS
+os.system('ver')
+
+# 5 - configurações da maquina
+os.system('systeminfo')
+
+# 6 - limpar a tela
+os.system('cls')
+
+
+
