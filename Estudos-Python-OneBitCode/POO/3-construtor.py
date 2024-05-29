e62c223a47042d810a5c4a195b868e8eb8d18c6b
Jhonatan Fagundes <149586964+Jhonatanfagundess@users.noreply.github.com>
2024-05-28 22:51:29 -0300

    Create 3-construtor.py

---
+++ Estudos-Python-OneBitCode/POO/3-construtor.py
@@ -0,0 +1,18 @@
+class Movie:
+    def __init__(self, name , yearLauch, plan , note, duraction ):
+        self.name = name
+        self.yearLauch = yearLauch
+        self.plan = plan
+        self.note = note
+        self.duraction = duraction
+        
+    def __str__(self):
+         return f'filme : {self.name} || Ano de Lançamento: {self.yearLauch}'  
+        
+        
+movie = Movie('Rush:No Limite da Emoção', 2013 , False , 10.0 , 92 )
+movie2 = Movie('Avengers:End Game', 2019 , False , 10.0 , 220)
+print(movie)        
+print(movie.name)
+print(movie2.name)
+print(movie2)
