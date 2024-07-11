0779cb660b23a6760ab6de6915663bcf5e16f211
Jhonatan Fagundes <149586964+Jhonatanfagundess@users.noreply.github.com>
2024-07-10 23:37:55 -0300

    att

---
+++ Estudos-Python-OneBitCode/Banco de Dados/10-att_dados.py
@@ -0,0 +1,8 @@
+from conexao_post import conn
+
+cursor_obj = conn.cursor(
+)
+
+sql = """
+    UPDATE game
+"""
--- Estudos-Python-OneBitCode/Banco de Dados/9-inserido_dados.py
+++ Estudos-Python-OneBitCode/Banco de Dados/9-inserido_dados.py
@@ -1,3 +1,17 @@
 from conexao_post import conn
 
 cursos_obj = conn.cursor()
+
+games = [
+    ('Resident Evil 4' , 2023 , 9.0),
+    ('The Last Of Us' , 2013 , 10)
+]
+
+for game in games:
+    cursos_obj.execute("""
+       INSERT into game(name , year , score)
+       VALUES (%s , %s , %s)                                        
+                       """, game)
+conn.commit()
+print('Dados Atualziados')
+conn.close()
